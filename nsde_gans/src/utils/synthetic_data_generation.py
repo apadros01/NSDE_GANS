e8ab38eb9b2b1961bf65a8967ece94d02805ca87
apadros01 <apadros01@hotmail.com>
2025-03-20 16:12:17 +0100

    send numpy arrays to cpu

--- nsde_gans/src/utils/synthetic_data_generation.py
+++ nsde_gans/src/utils/synthetic_data_generation.py
@@ -7,9 +7,9 @@ def sample_generator(generator,ts):
     samples = generator(ts, batch_size=1)
     samples = samples[0]
     samples = torch.abs(samples[-1])
-    return samples.detach().numpy()
+    return samples.detach().cpu().numpy()
 
-def synthtetic_dataset(generator,ts,df, size=3,): # by default, df = df_no_cvd.iloc[:,1:27]
+def synthtetic_dataset(generator,ts,df, size=3): # by default, df = df_no_cvd.iloc[:,1:27]
     samples=[]
     for i in range(size):
         samples.append(sample_generator(generator,ts))
